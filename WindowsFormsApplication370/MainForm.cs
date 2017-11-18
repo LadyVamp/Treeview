823fffe34c15ffcce79691d160e11caaf564e90d
ladyvamp <ladyvamp666@gmail.com>
2017-11-18 22:25:50 +0300

    TFile in metroGrid

--- WindowsFormsApplication370/MainForm.cs
+++ WindowsFormsApplication370/MainForm.cs
@@ -18,7 +18,6 @@ namespace Treeview
             ftMain.Build(new TreeNode());
         }
 
-
         private void ftMain_DoubleClick(object sender, EventArgs e)
         {
             var node = ftMain.SelectedNode as TreeNode;
@@ -42,6 +41,20 @@ namespace Treeview
         private void Build()
         {
             ftMain.Rebuild();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "repositoryDB2TFileWithoutId.TFile". При необходимости она может быть перемещена или удалена.
+            this.tFileTableAdapter.Fill(this.repositoryDB2TFileWithoutId.TFile);
+            metroGrid1.Columns[0].Width = 200; //name
+            metroGrid1.Columns[1].Width = 50; //type
+            metroGrid1.Columns[2].Width = 60; //date
+            metroGrid1.Columns[3].Width = 50; //size
+            metroGrid1.Columns[4].Width = 100; //keywords
+            metroGrid1.Columns[5].Width = 200; //fileContent
+            metroGrid1.Columns[6].Width = 60; //subcatalog
+
         }
     }
 }
