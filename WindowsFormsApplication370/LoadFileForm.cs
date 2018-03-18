12ed3d53dc63019de4af4aa367daf8e791b82eb7
ladyvamp <ladyvamp666@gmail.com>
2018-03-18 17:57:12 +0300

    del unnecessary using

--- WindowsFormsApplication370/LoadFileForm.cs
+++ WindowsFormsApplication370/LoadFileForm.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
-using System.Xml;
-using System.Net;
 
 namespace Treeview
 {
